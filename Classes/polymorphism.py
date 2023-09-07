df35684157300315ec0ca0a8dc50aeb584298153
iamabhaytiwari343 <108920327+iamabhaytiwari343@users.noreply.github.com>
2023-09-07 22:27:58 +0530

    polymorphism

--- Classes/polymorphism.py
+++ Classes/polymorphism.py
@@ -13,3 +13,27 @@ class Dog(Animal):
 class Cat(Animal):
     def speak(self):
         return "Meow!"
+
+
+class Duck(Animal):
+    def speak(self):
+        return "Quack!"
+
+# Function that uses polymorphism
+
+
+def animal_sound(animal):
+    return animal.speak()
+
+
+# Create instances of different animal classes
+dog = Dog()
+cat = Cat()
+duck = Duck()
+
+# Call the function with different animal objects
+print("Dog says:", animal_sound(dog))
+print("Cat says:", animal_sound(cat))
+print("Duck says:", animal_sound(duck))
+
+# Polymorphism is one of the four fundamental principles of object-oriented programming (OOP), along with encapsulation, inheritance, and abstraction. It allows objects of different classes to be treated as objects of a common superclass. In Python, polymorphism is achieved through method overriding, where a subclass provides a specific implementation of a method that is already defined in its superclass. Polymorphism simplifies code and makes it more flexible and extensible.
